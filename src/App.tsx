1fea51c6708da13bf526a3a8f63cd53c8c45673c
yiss <ilias.yahia1@gmail.com>
2023-06-08 14:23:20 +0200

    2 files modified, 5 added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { detectBrowser, fingerprintUsingCanvas } from './uaDetect';
+import { fingerprintUsingCanvas } from './uaDetect';
 
 function App() {
   const [stuff, setStuff] = useState('nothing');
